4374b4e907faab7b0e58c15bee1e2bc62b3bbf7a
min-96 <ajdcnd131@naver.com>
2024-08-27 12:21:45 +0900

    feat: accessToken filter

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.api"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // implementation("com.nimbusds:nimbus-jose-jwt:9.31")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/api/apigateway/util/JwtAuthenticationFilter.kt
@@ -0,0 +1,44 @@
+package com.api.apigateway.util
+
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.GatewayFilterChain
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class JwtAuthenticationFilter(private val jwtProvider: JwtProvider) : GatewayFilter {
+
+    override fun filter(exchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> {
+        val request = exchange.request
+        val token = request.headers.getFirst(HttpHeaders.AUTHORIZATION)?.removePrefix("Bearer ")
+
+        return if (token != null && jwtProvider.validateToken(token)) {
+            val claims = jwtProvider.getClaimsFromToken(token)
+            if (claims != null) {
+                val address = claims.getClaim("address") as? String
+                if (address != null) {
+                    val modifiedRequest = exchange.request.mutate()
+                        .header("X-Auth-Address", address)
+                        .build()
+
+                    chain.filter(exchange.mutate().request(modifiedRequest).build())
+                } else {
+                    onError(exchange, "Address claim not found in token")
+                }
+            } else {
+                onError(exchange, "Invalid token claims")
+            }
+        } else {
+            onError(exchange, "Invalid or missing token")
+        }
+    }
+
+    private fun onError(exchange: ServerWebExchange, err: String): Mono<Void> {
+        val response = exchange.response
+        response.statusCode = HttpStatus.UNAUTHORIZED
+        return response.writeWith(Mono.just(response.bufferFactory().wrap(err.toByteArray())))
+    }
+}
